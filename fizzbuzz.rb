c26ffe702ef3f722785075624027320779a6f2fc
Enrique Carral Trevino <ecarralt@gmail.com>
2016-06-08 17:14:54 -0500

    modified version with no classes and shorter, uses elsifs though

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,29 +1,40 @@
-class PrintNumbers
 
-  @count = 1
 
-  while @count <= 100
-    case
-    when @count%3==0
-      if @count%5==0
-        print "FizzBuzz,"
-      else
-      end
-      print "Fizz, "
-    when @count%5==0
-      if @count%3==0
-        print "FizzBuzz, "
-      else
-      end
-      print "Buzz, "
-    else
-      print "#{@count}, "
-    end
-
-    @count += 1
-
+(1..100).each do |n|
+  if n%3==0 && n%5==0
+    print "FizzBuzz"
+  elsif n%3==0
+    print "Fizz"
+  elsif n%5==0
+    print "Buzz"
+  else
+    print n
   end
-
 end
 
-PrintNumbers
+
+
+### Below code remains from an attempt using classes (it wuold have to be wrapped within a class)
+
+
+  # while @count <= 100
+  #   case
+  #   when @count%3==0
+  #     if @count%5==0
+  #       print "FizzBuzz,"
+  #     else
+  #     end
+  #     print "Fizz, "
+  #   when @count%5==0
+  #     if @count%3==0
+  #       print "FizzBuzz, "
+  #     else
+  #     end
+  #     print "Buzz, "
+  #   else
+  #     print "#{@count}, "
+  #   end
+
+    # @count += 1
+
+#   end
